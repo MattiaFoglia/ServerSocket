458982ba28013e7c3bbb1aedf4f4587bf1eec29f
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 10:38:36 +0200

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 
 public class Main {
-    
+     
     public static void main(String[] args) throws IOException {
         System.out.println("Server Avviato!");
         ServerSocket ss = new ServerSocket(4316);
